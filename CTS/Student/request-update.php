3a1cb32711fbfd11f49c8d6b6e7d85e5b11d60d8
tacohwa <77439572+tacohwa@users.noreply.github.com>
2024-08-29 14:56:34 +0800

    request-update.php

--- CTS/Student/request-update.php
+++ CTS/Student/request-update.php
@@ -65,7 +65,7 @@ if (isset($_POST['save'])) {
 
   <main id="main" class="main">
     <div class="pagetitle">
-      <h1>Form Layouts</h1>
+      <h1>Request Update Teaching Plan Form</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.php">Home</a></li>
